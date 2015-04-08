1cd144135a3b78b033af78f9d442bf53fd429059
Kyle Fuller <kyle@fuller.li>
2015-04-08 16:18:17 +0100

    Initial Xcode project

---
+++ Hyperdrive/Hyperdrive.h
@@ -0,0 +1,19 @@
+//
+//  Hyperdrive.h
+//  Hyperdrive
+//
+//  Created by Kyle Fuller on 08/04/2015.
+//  Copyright (c) 2015 Apiary. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Hyperdrive.
+FOUNDATION_EXPORT double HyperdriveVersionNumber;
+
+//! Project version string for Hyperdrive.
+FOUNDATION_EXPORT const unsigned char HyperdriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hyperdrive/PublicHeader.h>
+
+
