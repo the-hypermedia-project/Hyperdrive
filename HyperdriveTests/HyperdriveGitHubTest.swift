bd588ced1e49caf127231f49a922f53c71289976
Z <z@apiary.io>
2015-08-08 16:02:56 +0200

    GitHub & hyperdrive origin

---
+++ HyperdriveTests/HyperdriveGitHubTest.swift
@@ -0,0 +1,40 @@
+//
+//  HyperdriveGitHubTest.swift
+//  Hyperdrive
+//
+//  Created by Z on 8/8/15.
+//  Copyright (c) 2015 Apiary. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Representor
+import Hyperdrive
+
+class GitHubAdapterTests: XCTestCase {
+
+  func testGitHubRoot() {
+    let hyperdrive = Hyperdrive()
+    
+    let expectation = expectationWithDescription("...")
+    
+    hyperdrive.enter("https://api.github.com/") { result in
+      switch result {
+      case .Success(let representor):
+        println("The API has offered us the following transitions: \(representor.transitions)")
+        
+      case .Failure(let error):
+        println("Unfortunately there was an error: \(error)")
+      }
+      
+      expectation.fulfill()
+    }
+    
+    waitForExpectationsWithTimeout(10) { error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+      }
+    }
+  }
+  
+}
